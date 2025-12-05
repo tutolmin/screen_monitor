3a9348835d14e3052601a88dc18c43e157988d90
Andrei Tutolmin <tutolmin@gmail.com>
2025-12-05 14:27:42 +0300

    Yandex IAM token from JWT

--- tokens/iam.py
+++ tokens/iam.py
@@ -1,8 +1,13 @@
+import yandexcloud
+
+from yandex.cloud.iam.v1.iam_token_service_pb2 import (CreateIamTokenRequest)
+from yandex.cloud.iam.v1.iam_token_service_pb2_grpc import IamTokenServiceStub
+
 import time
 import jwt
 import json
 
-key_path = '<JSON-файл_c_ключами>'
+key_path = '../keys/authorized_key.json'
 
 # Чтение закрытого ключа из JSON-файла
 with open(key_path, 'r') as f:
@@ -17,6 +22,20 @@ sa_key = {
     "service_account_id": service_account_id,
     "private_key": private_key
 }
+
+def create_iam_token():
+  jwt = create_jwt()
+  
+  sdk = yandexcloud.SDK(service_account_key=sa_key)
+  iam_service = sdk.client(IamTokenServiceStub)
+  iam_token = iam_service.Create(
+      CreateIamTokenRequest(jwt=jwt)
+  )
+  
+  print("Your iam token:")
+  print(iam_token.iam_token)
+
+  return iam_token.iam_token
 
 def create_jwt():
     now = int(time.time())
@@ -39,4 +58,5 @@ def create_jwt():
 
     return encoded_token
 
-create_jwt()
+#create_jwt()
+create_iam_token()
